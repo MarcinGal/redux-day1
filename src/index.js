3796228bd28a21055a8d2425b81ab98775ecb79f
marcingal <marcin921125@gmail.com>
2018-12-03 10:29:39 +0100

    added store

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+//REDUX IMPORTS
 import { combineReducers, createStore } from 'redux'
+import todos from './store'
 
+//REDUX GO HERE
+const rootReducer = combineReducers({
+    todos
+})
 
-const rootReducer = combineReducers({})
 const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
