1053748b889a7585dd792c77a4f1fb3caea43362
marcingal <marcin921125@gmail.com>
2018-12-03 10:23:15 +0100

    added redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { combineReducers, createStore } from 'redux'
+
+
+const rootReducer = combineReducers({})
+const store = createStore(rootReducer)
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
 
