95ccaf8aecda2d726387eaa1abec01a54b6b63be
marcingal <marcin921125@gmail.com>
2018-12-03 10:59:47 +0100

    redux chrome extension added

--- src/index.js
+++ src/index.js
@@ -13,7 +13,10 @@ const rootReducer = combineReducers({
     todos
 })
 
-const store = createStore(rootReducer)
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__())
 
 console.warn(store.getState())
 
