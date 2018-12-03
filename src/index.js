e4247134dbdca810e46d6c502399d7407c574c36
marcingal <marcin921125@gmail.com>
2018-12-03 14:47:30 +0100

    added cases to todos

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 //REDUX IMPORTS
 import { combineReducers, createStore } from 'redux'
-import todos, { addTodo } from './store/toDos'
+import todos, { addTodo, filterTodos, toggleTodo, deletedTodos } from './store/toDos'
 import counter, { increment, decrement, reset } from './store/counter'
 import cart, { addToCart } from './store/cart'
 
@@ -31,11 +31,15 @@ store.dispatch(increment())
 store.dispatch(decrement())
 store.dispatch(reset())
 store.dispatch(addToCart())
+store.dispatch(deletedTodos())
 
 window.letIncreaseCounter = () => store.dispatch(increment())
 window.justAddTodo = (text) => store.dispatch(addTodo(text))
 window.letDecreaseCounter = () => store.dispatch(decrement())
 window.addPriceToCart = (price) => store.dispatch(addToCart(price))
+window.filterTodos = (input) => store.dispatch(filterTodos(input))
+window.toggleTodo = index => store.dispatch(toggleTodo(index))
+window.deletedTodos = index => store.dispatch(deletedTodos(index))
 
 // console.warn(store.getState())
 
--- src/store/toDos.js
+++ src/store/toDos.js
@@ -1,25 +1,59 @@
 const ADD_TODO = 'ADD_TODO'
-
+const FILTERED_TODO = 'FILTERED=TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const DELETE_TODO = 'DELETE_TODO'
+const INITIAL_STATE = {
+    allTodos: [],
+    visibleTodos: []
+}
 
 export const addTodo = text => ({
     type: ADD_TODO,
     text
 })
 
-export default (state = [], action) => {
+export const filterTodos = text => ({
+type: FILTERED_TODO,
+input: text
+})
+
+export const toggleTodo = text => ({
+    type: TOGGLE_TODO,
+    input: text
+})
+
+export const deletedTodos = text => ({
+    type: DELETE_TODO,
+    input: text
+})
+
+export default (state = INITIAL_STATE, action) => {
     switch (action.type) {
         case ADD_TODO:
             const newTodo = { text: action.text, completed: false }
-            return [...state, newTodo]
-        case 'SWITCH_TODO_COMPLETED':
-            return [
-                ...state.slice(0, action.index),
-                {
-                    text: state[action.index].text,
-                    completed: !state[action.index].completed
-                },
-                ...state.slice(action.index + 1)
-            ]
+            // [...state, newTodo]
+            return {
+                ...state,
+                allTodos: [...state.allTodos, newTodo]
+            }
+            case FILTERED_TODO:
+            return {
+                ...state,
+                visibleTodos: state.allTodos.filter(todo => todo.text.includes(action.input))
+            }
+            case TOGGLE_TODO:
+            return {
+                ...state,
+                allTodos: state.allTodos.map((todo, index) =>(index === action.input) ?
+                {...todo, completed: !todo.completed }
+                   : todo
+                )
+            }
+            case DELETE_TODO:
+            return {
+                ...state,
+                allTodos: state.allTodos.filter((todo, index) => !(index === action.input))
+            }
         default:
             return state
     }
