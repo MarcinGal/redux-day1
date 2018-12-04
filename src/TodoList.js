6582e6b40e31fa711d30f79ae5786a8ef6836ff8
marcingal <marcin921125@gmail.com>
2018-12-04 11:08:49 +0100

    added clickable interaction on listItems

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,18 +1,19 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import { addTodo } from './store/todos'
+import { addTodo, toggleTodo } from './store/todos'
 
 const mapStateToProps = store => ({
     _todos: store.todos.allTodos
 })
 
 const mapDispatchToProps = dispatch => ({
-    _addTodo: text => dispatch(addTodo(text))
+    _addTodo: text => dispatch(addTodo(text)),
+    _toggleTodo: index => dispatch(toggleTodo(index))
 })
 
 class TodoList extends React.Component {
-    state = { value: ''}
+    state = { value: '' }
 
     handleInputChange = event => {
         this.setState({ value: event.target.value })
@@ -24,14 +25,21 @@ class TodoList extends React.Component {
         this.props._addTodo(this.state.value)
     }
 
+    handleTodoClick = index => {
+        this.props._toggleTodo(index)
+    }
 
     render() {
         return (
             <div>
                 <input onChange={this.handleInputChange} />
                 <button onClick={this.handleButtonClick}>Add todo</button>
-                {this.props._todos.map(todo =>
-                    <div key={todo.text}>{todo.text}</div>
+                {this.props._todos.map((todo, index) =>
+                    <div
+                        style={{ textDecoration: todo.completed ? 'underline' : 'none' }}
+                        onClick={() => this.handleTodoClick(index)}
+                        key={todo.text}>{todo.text}
+                    </div>
                 )}
             </div>
         )
