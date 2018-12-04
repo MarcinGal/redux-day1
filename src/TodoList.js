b5c4340f08d5dfa6246a595eac334da26af52330
marcingal <marcin921125@gmail.com>
2018-12-04 10:14:04 +0100

    wip stte

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,11 +1,24 @@
 import React from 'react'
 
 class TodoList extends React.Component {
+state = {}
+
+handleInputChange = event => {
+    this.setState({ value: event.target.value})
+}
+
+handleButtonClick = () => {
+    console.log('want to save todo:', this.state.value)
+}
+
+
     render() {
         return (
             <div>
+                <input onChange={this.handleInputChange} />
+                <button onClick={this.handleButtonClick}>Add todo</button>
                 {this.props.todos.map(todo =>
-                    <div>{todo.text}</div>
+                    <div key={todo.text}>{todo.text}</div>
                 )}
             </div>
         )
